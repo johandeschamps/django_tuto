df5379bb3b746f57a542bffd94c372875a04509f
Johan Deschamps <johan.deschamps@fms-ea.com>
2024-10-09 10:36:43 +0200

    add search_fields and list_filter to class

--- polls/admin.py
+++ polls/admin.py
@@ -5,9 +5,13 @@ from .models import Choice
 
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ('question_text', 'pub_date')
+    search_fields = ('question_text',)
+    list_filter = ('pub_date',)
 
 class ChoiceAdmin(admin.ModelAdmin):
-    list_display = ('choice_text', 'votes', 'question') 
+    list_display = ('choice_text', 'votes', 'question')
+    search_fields = ('choice_text',)
+    list_filter = ('votes', 'question')
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice, ChoiceAdmin)
