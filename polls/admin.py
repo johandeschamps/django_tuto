d6afc069e3450227d854015d7752073dbf325330
Johan Deschamps <johan.deschamps@fms-ea.com>
2024-10-09 10:08:39 +0200

    update db.sqlite3

--- polls/admin.py
+++ polls/admin.py
@@ -3,5 +3,11 @@ from django.contrib import admin
 from .models import Question
 from .models import Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date')  # Ajoute les attributs ici
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('choice_text', 'votes', 'question')  # Ajoute les attributs ici
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
