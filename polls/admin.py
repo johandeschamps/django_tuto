7a9cceb2466d52eabc9b2e50860c26c0a2bfb8eb
Johan Deschamps <johan.deschamps@fms-ea.com>
2024-10-09 10:10:41 +0200

    update

--- polls/admin.py
+++ polls/admin.py
@@ -4,10 +4,10 @@ from .models import Question
 from .models import Choice
 
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ('question_text', 'pub_date')  # Ajoute les attributs ici
+    list_display = ('question_text', 'pub_date')
 
 class ChoiceAdmin(admin.ModelAdmin):
-    list_display = ('choice_text', 'votes', 'question')  # Ajoute les attributs ici
+    list_display = ('choice_text', 'votes', 'question') 
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice, ChoiceAdmin)
