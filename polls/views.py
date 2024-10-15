20ca63883a291a5b8870de9ce2c68a6e6f885a1a
Johan Deschamps <johan.deschamps@fms-ea.com>
2024-10-15 09:52:13 +0200

    add all page for display all polls

--- polls/views.py
+++ polls/views.py
@@ -45,3 +45,7 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+
+def polls_list(request):
+    polls = Question.objects.all()
+    return render(request, 'polls/all.html', {'polls': polls})
